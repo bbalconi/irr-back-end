2dbc757f9b4d2f86d4bfff25aeccfe4abf10861c
kramduckner <buckner.a.mark@gmail.com>
2017-11-30 13:01:33 -0700

    added deps and login route

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-var express = require("express");
-var app = express();
-var bodyParser = require('body-parser');
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const { Pool, Client } = require('pg');
+const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
 
 app.use(express.static('public'));
 app.use(bodyParser.json({type:'application/json'}));
 app.use(bodyParser.urlencoded({extended:true}));
 
+const pool = new Pool({
+  user: 'mark',
+  host: 'localhost',
+  database: 'irr',
+  password: process.env.databasePw,
+  port: 5432,
+});
+
+app.post("/login", function(req, res){
+  console.log(req.body);
+});
 
 app.get("/", function(req, res) {
-    res.sendfile('index.html');
+  res.sendfile('index.html');
 });
 
 app.listen(5000, function() {
-   console.log("Listening on 5000");
+  console.log("Listening on 5000");
 });
