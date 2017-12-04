1d3fcfc7603c8a7cc38ebc43409c70eee950071a
kramduckner <buckner.a.mark@gmail.com>
2017-12-04 10:59:57 -0700

    added jwt and incorporated into authenticate handler

--- server.js
+++ server.js
@@ -4,21 +4,46 @@ const bodyParser = require('body-parser');
 const { Pool, Client } = require('pg');
 const morgan = require('morgan');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 app.use(express.static('public'));
 app.use(bodyParser.json({type:'application/json'}));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan('dev'));
+app.set('superSecret', "hmmm");
 
 const pool = new Pool({
   user: 'mark',
   host: 'localhost',
   database: 'irr',
-  password: process.env.databasePw,
+  password: process.env.psqlPW,
   port: 5432,
 });
 
-app.post("/login", function(req, res){
-  console.log(req.body);
+// pool.query('SELECT * from users', (err, res) => {
+//   console.log(err, res);
+//   //pool.end()
+// });
+
+app.post("/authenticate", function(req, res){ 
+  pool.connect().then((client, done)=>{
+    client.query(`select * from users where email='${req.body.email}'`).then((res)=>{
+      //can put more claims into this user obj
+      var user = {
+        user:res.rows[0].email
+      };
+      var token = jwt.sign(user, app.get('superSecret'), {
+        //expiresInMinutes: 1440 // expires in 24 hours, no longer valid, probs deprecated
+      });
+      res.json({
+        message:"success",
+        token:token
+      });
+    });
+  }, (err)=>{
+    console.log(err);
+  });
+  res.json('dude');
 });
 
 app.get("/", function(req, res) {
